35b050f749b0c2421b149f09ea97c96c4f89af0c
Riya Gupta <riya.rg3010@gmail.com>
2026-03-29 12:50:49 +0530

    about us

--- app/api/partners/[id]/route.ts
+++ app/api/partners/[id]/route.ts
@@ -1,15 +1,17 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function DELETE(
-    request: Request,
-    { params }: { params: { id: string } },
-) {
+export async function DELETE(request: Request, context: any) {
+    const id = context.params.id;
+
     try {
         await prisma.partner.delete({
-            where: { id: params.id },
+            where: { id },
         });
-        return NextResponse.json({ message: "Partner deleted successfully" });
+
+        return NextResponse.json({
+            message: "Partner deleted successfully",
+        });
     } catch (error) {
         return NextResponse.json(
             { error: "Failed to delete partner" },
